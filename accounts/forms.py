f91cd7d912e1ee0e384798e932cf6876f110a9a3
jumashafara <jumashafara0@gmail.com>
2023-10-15 10:15:38 +0300

    rebuilt signup page

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,15 +4,33 @@ from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
     username = forms.CharField(
+        label='',
         required=True,
         widget=forms.TextInput(
             attrs={'placeholder': 'username', 
                    'class': 'block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input'}), 
         )
     email = forms.CharField(
+        label='',
         required=True,
         widget=forms.EmailInput(
-            attrs={'placeholder': 'username', 
+            attrs={'placeholder': 'email', 
+                   'class': 'block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input'}), 
+        )
+    
+    password1 = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'password', 
+                   'class': 'block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input'}), 
+        )
+    
+    password2 = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'confirm password', 
                    'class': 'block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input'}), 
         )
 
@@ -36,7 +54,8 @@ class CustomUserAuthenticationForm(AuthenticationForm):
             attrs={'placeholder': 'password', 
                    'class': 'block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input'}), 
         )
+    
 
     class Meta:
         model = User  # Replace 'User' with your custom User model if you have one
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1',)
