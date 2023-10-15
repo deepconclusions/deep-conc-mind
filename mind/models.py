c50191303ee82db55ea7b7765e8af5c52af6bd8e
jumashafara <jumashafara0@gmail.com>
2023-10-15 16:33:36 +0300

    saved state

--- mind/models.py
+++ mind/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+
 # Create your models here.
 User = get_user_model()
 
 class Chat(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    message = models.TextField()
-    response = models.TextField()
+    human = models.TextField()
+    mind_bot = models.TextField()
     def __str__(self):
         return self.user.username
     
--- mind/views.py
+++ mind/views.py
@@ -7,15 +7,16 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferWindowMemory
-from langchain.memory import ConversationBufferMemory
-from langchain.chains.conversation.memory import ConversationBufferMemory
+# from langchain.memory import ConversationBufferMemory
+# # from langchain.chains.conversation.memory import ConversationBufferMemory
 
 from .models import Chat
 from .models import Secret
 
 # Create your views here.
 
-memory = ConversationBufferMemory()
+memory = ConversationBufferWindowMemory()
+
 def askOpenAI(message:str):
     os.environ['OPENAI_API_KEY'] = Secret.objects.get(name='openai_key').secret
     llm = OpenAI(temperature=0)
@@ -27,11 +28,6 @@ def askOpenAI(message:str):
                    If the bot does not know the answer to a question, 
                    it truthfully says it does not know.\n\nCurrent conversation:\n{history}\nHuman: {input}\nDeep Conclucions Mind bot:'),
                     """)
-    # prompt = PromptTemplate(
-    # input_variables =['message'],
-    # output_variables =['response'],
-    # template = "Please respond kindly: {message}"
-    # )
 
     
     chain = ConversationChain(llm=llm,prompt=prompt, memory=memory)
@@ -43,22 +39,23 @@ def askOpenAI(message:str):
 
 def mind(request):
     if request.user.is_authenticated:
+        chat = Chat.objects.filter(user=request.user)
         if request.method == 'POST':
             message = request.POST.get('message')
             clean_message = str(message)
-            response = askOpenAI(clean_message)
+
+            response = askOpenAI(message=clean_message)
             
             # save chat to database
-            chat = Chat(user=request.user, message=message, response=response)
+            chat = Chat(user=request.user, human=message, mind_bot=response)
             chat.save()
 
 
             return redirect(request.META['HTTP_REFERER']) 
         else:
             # get all chats from database
-            chats = Chat.objects.filter(user=request.user)
-            context = {'history': chats}
-            template_name = 'mind.html'
+            context = {'history': chat}
+            template_name = 'mind/mind.html'
             return render(request=request,
                           template_name=template_name,
                            context=context)
